74004f0fbf1e1fb256f82824529a9b945fdbcbea
Krittithee Tuncharoen <maikittitee@Krittithees-MacBook-Pro.local>
2022-08-23 15:38:26 +0700

    lab04done

--- lab/lab04/numchar.c
+++ lab/lab04/numchar.c
@@ -17,19 +17,23 @@ void	numchar(char *str)
 	n = 0;
 	while (str[i])
 	{
+		//printf("bp 20 i = %d , str[i] = %c , n = %d\n",i,str[i],n);
 		if (isnumber(str[i]))
 		{
 			n *= 10;
 			n += str[i] - '0';
+		//	printf("bp 25 i = %d , str[i] = %c , n = %d\n",i,str[i],n);
 		}
 		else
 		{
+			//printf("bp 27 i = %d , str[i] = %c, str[i - 1] = %c, n = %d\n",i,str[i],str[i - 1], n);
 			if (str[i - 1] && isnumber(str[i - 1]))
 				loop_char(str[i], n);
 			else
 				printf("%c", str[i]);
 			n = 0;
 		}
+		//printf("- - - - - - \n");
 		i++;
 
 	}
@@ -37,10 +41,67 @@ void	numchar(char *str)
 
 int	main(int argc, char **argv)
 {
-	char	str[150];
+	if (argc <= 1)
+	{
+		char	str[150];
 
-	printf("Enter String : ");
-	scanf("%s",str);
-	numchar(str);
+		printf("Enter String : ");
+		scanf("%s",str);
+		numchar(str);
+	}
+	else if (argc == 2)
+		numchar(argv[1]);
+	else
+	{
+
+			printf("input : 3A2D\n");
+			printf("expect output :AAADD \n");
+			printf("Program output : \n");
+			numchar("3A2D");
+			printf("\n");
+			printf("----------\n");
+			printf("input : A3D\n");
+			printf("expect output :ADDD \n");
+			printf("Program output : \n");
+			numchar("A3D");
+			printf("\n");
+			printf("----------\n");
+			printf("input : A12D\n");
+			printf("expect output :ADDDDDDDDDDDD \n");
+			printf("Program output : \n");
+			numchar("A12D");
+			printf("\n");
+			printf("----------\n");
+			printf("input : 12Dkg\n");
+			printf("expect output :DDDDDDDDDDDDkg \n");
+			printf("Program output :\n ");
+			numchar("12Dkg");
+			printf("\n");
+			printf("----------\n");
+			printf("input : abc\n");
+			printf("expect output :abc \n");
+			printf("Program output : \n");
+			numchar("abc");
+			printf("\n");
+			printf("----------\n");
+			printf("input : HE2LO\n");
+			printf("expect output :HELLO \n");
+			printf("Program output : \n");
+			numchar("HE2LO");
+			printf("\n");
+			printf("----------\n");
+			printf("input : 1234\n");
+			printf("expect output : [null] \n");
+			printf("Program output : \n");
+			numchar("1234");
+			printf("\n");
+			printf("----------\n");
+			printf("input : 2147483648A\n");
+			printf("expect output : {Error]\n");
+			printf("Program output : \n");
+			numchar("2147483648A");
+			printf("\n");
+			printf("----------\n");
+	}
 }
 
