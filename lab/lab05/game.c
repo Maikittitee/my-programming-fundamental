fb8733158c72af577004d27b9c067b61b553d654
ktunchar <maikittitee1615@gmail.com>
2022-09-12 21:46:34 +0700

    lab05 done

--- lab/lab05/game.c
+++ lab/lab05/game.c
@@ -10,22 +10,61 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdio.h>
+#include <stdio.h>
 #include <windows.h>
+#include <conio.h>
 
 void gotoxy(int x, int y)
 {
 	COORD c = { x, y };
-	SetConsoleCursorPosition(
-	GetStdHandle(STD_OUTPUT_HANDLE) , c);
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE) , c);
 }
-void	draw_ship()
+void	draw_ship(int x, int y)
 {
+	gotoxy(x,y);
 	printf(" <-0-> ");
+	//printf("(%d,%d)",x,y);
 }
 
-int	main(void)
+void	ease_ship(int x, int y)
 {
-	draw_ship();
+	gotoxy(x,y);
+	printf("       ");
+}
+
+int main()
+{
+	char ch=' ';
+	int x=35,y=20;
+	draw_ship(x,y);
+	do 
+	{
+		if (_kbhit())
+		{
+			ch=_getch();
+			if(ch=='a' && x > 0) 
+			{
+				ease_ship(x,y);
+				draw_ship(--x,y);
+			}
+			if(ch=='d' && x < 80) 
+			{
+				ease_ship(x,y);
+				draw_ship(++x,y);
+			}
+			if(ch=='w' && y > 0) 
+			{
+				ease_ship(x,y);
+				draw_ship(x,--y);
+			}
+			if(ch=='s' && y < 20) 
+			{
+				ease_ship(x,y);
+				draw_ship(x,++y);
+			}
+			fflush(stdin);
+		}
+		Sleep(100);
+	} while (ch!='x');
 	return 0;
 }
